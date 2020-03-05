5e8093f02f1e46c6ec73fe5646a6db953244f53f
neocheekiong <neocheekiong@gmail.com>
2020-03-05 21:38:28 +0800

    created more game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,7 +1,15 @@
-let cardOne = "queen"
-let cardTwo = "king"
-let cardThree = "king"
-let cardFour = "queen"
+let cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
+
+let cardOne = cards[3];
+cardsInPlay.push(cardOne);
 
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+
+let cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo)
+
+if (cardsInPlay.length === 2) {
+    cardsInPlay[0] === cardsInPlay [1] ? alert("You found a match!") : alert("Sorry, try again.");
+}
